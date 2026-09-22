ee5d1227a41788a1d118a3aff226eb5299e1e370
ViniluR <vi.he.de.so.pi@gmail.com>
2026-09-22 20:47:23 -0300

    Dashboard AdafruitIO completamente implementado

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,22 @@
 
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883  // use 8883 for SSL
-#define AIO_USERNAME    "ViniciusPi"
-#define AIO_KEY         "aio_DmPw48ShZ0kHR0OsY6XIGv52GaYW"
+#define AIO_USERNAME    "" //colocar aqui o nome de usuario do adafruit
+#define AIO_KEY         "" //colocar aqui a chave do adafruit
 
 WiFiClient client;
 
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
 Adafruit_MQTT_Publish vagasPub = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/estacionamento.vagas-status");
-Adafruit_MQTT_Subscribe vagasSub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/estacionamento.vagas-comandos");
+
+Adafruit_MQTT_Subscribe vagaIdSub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/estacionamento.vagas-id");
+Adafruit_MQTT_Subscribe vagaOnOffSub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/estacionamento.vagas-onoff");
+
+Adafruit_MQTT_Publish vaga1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/estacionamento.vaga-1");
+Adafruit_MQTT_Publish vaga2 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/estacionamento.vaga-2");
+Adafruit_MQTT_Publish vaga3 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/estacionamento.vaga-3");
+Adafruit_MQTT_Publish vaga4 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/estacionamento.vaga-4");
 
 LiquidCrystal_I2C display(0x27, 16, 2);
 
@@ -31,8 +39,10 @@ LiquidCrystal_I2C display(0x27, 16, 2);
 #define COR_AZUL 2
 #define COR_AMARELA 3
 
-unsigned long lastMsg = 0;
-int vagasStatus[NUM_VAGAS][2] = {{0, 0}, {1, 0}, {2, 0}, {3, 0}};
+int idVagaRecebido = -1;
+
+int contadorPublicado = -1;
+int statusPublicado[NUM_VAGAS] = {-1, -1, -1, -1};
 
 const int ledsVagas[NUM_VAGAS][3] = {
   {13, 12, 14},
@@ -44,6 +54,7 @@ const int ledsVagas[NUM_VAGAS][3] = {
 int trig_vagas[] = {33, 32, 5, 17};
 int echo_vagas[] = {35, 34, 36, 39};
 bool vagaOcupada[NUM_VAGAS] = {false, false, false, false};
+bool vagaBloqueada[NUM_VAGAS] = {false, false, false, false};
 
 byte simboloLivre[8] = {
   B00000, B00001, B00010, B10100, B01000, B00000, B00000, B00000
@@ -77,43 +88,81 @@ void MQTT_connect() {
     delay(5000);
     retries--;
     if (retries == 0) {
-      while (1);
+      Serial.println("MQTT unavailable; trying again later");
+      return;
     }
   }
   Serial.println("MQTT Connected!");
 }
 
-void vagas_callback(char *data, uint16_t len) {
+bool publicarVagaStatus(int vaga) {
+  if (vaga < 0 || vaga >= NUM_VAGAS) {
+    return false;
+  }
+
+  int statusAtual = vagaOcupada[vaga] ? 1 : 0;
+
+  if (statusPublicado[vaga] == statusAtual) return true;
+
+  bool sucesso;
+  switch (vaga) {
+    case 0:
+      sucesso = vaga1.publish(statusAtual ? "1" : "0");
+      break;
+    case 1:
+      sucesso = vaga2.publish(statusAtual ? "1" : "0");
+      break;
+    case 2:
+      sucesso = vaga3.publish(statusAtual ? "1" : "0");
+      break;
+    default:
+      sucesso = vaga4.publish(statusAtual ? "1" : "0");
+      break;
+  }
+
+  if (sucesso) statusPublicado[vaga] = statusAtual;
+  return sucesso;
+}
+
+void vagaIdCallback(char *data, uint16_t len) {
   data[len] = '\0';
-
-  int idVaga;
-  int ocupado;
-
-  sscanf(data, "%d,%d", &idVaga, &ocupado);
+  int idVaga = atoi(data) - 1;
 
   if (idVaga >= 0 && idVaga < NUM_VAGAS) {
-    vagaOcupada[idVaga] = (ocupado == 1);
-    Serial.print("Vaga ");
-    Serial.print(idVaga);
-    Serial.print(" -> ");
-    Serial.println(vagaOcupada[idVaga] ? "Ocupada" : "Livre");
+    idVagaRecebido = idVaga;
   }
 }
 
-void publicarVagasStatus() {
-  int vagasLivres = 0;
+void vagaOnOffCallback(char *data, uint16_t len) {
+  data[len] = '\0';
+  int manual = atoi(data);
 
-  for (int i = 0; i < NUM_VAGAS; i++) {
-    if (!vagaOcupada[i]) {
-      vagasLivres++;  
+  if (idVagaRecebido >= 0 && idVagaRecebido < NUM_VAGAS) {
+    if (vagaBloqueada[idVagaRecebido] != (manual == 1)) {
+      vagaOcupada[idVagaRecebido] = (manual == 1);
+      vagaBloqueada[idVagaRecebido] = vagaOcupada[idVagaRecebido];
+    
+      Serial.print("Vaga ");
+      Serial.print(idVagaRecebido + 1);
+      Serial.print(" -> ");
+      Serial.println(vagaBloqueada[idVagaRecebido] ? "Manual" : "Automática");
     }
   }
+}
 
-  String mqtt_msg = String(vagasLivres);
-  if (!vagasPub.publish(mqtt_msg.c_str())) {
-    Serial.println("Falha ao publicar");
-  } else {
-    Serial.println("Publicado com sucesso");
+void publicarAlteracoes(int vagasLivres) {
+  for (int i = 0; i < NUM_VAGAS; i++) {
+    publicarVagaStatus(i);
+  }
+
+  if (contadorPublicado != vagasLivres) {
+    if (vagasPub.publish(vagasLivres)) {
+      contadorPublicado = vagasLivres;
+      Serial.print("Status atualizado, vaga(s) livre(s): ");
+      Serial.println(vagasLivres);
+    } else {
+      Serial.println("Falha ao publicar status das vagas");
+    }
   }
 }
 
@@ -159,8 +208,10 @@ void setup() {
 
   conectarWifi();
 
-  vagasSub.setCallback(vagas_callback);
-  mqtt.subscribe(&vagasSub);
+  vagaIdSub.setCallback(vagaIdCallback);
+  vagaOnOffSub.setCallback(vagaOnOffCallback);
+  mqtt.subscribe(&vagaIdSub);
+  mqtt.subscribe(&vagaOnOffSub);
 
   display.init();
   display.backlight();
@@ -196,18 +247,30 @@ void setup() {
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
+    bool estavaConectado = mqtt.connected();
     MQTT_connect();
-    mqtt.processPackets(10000);
-    if (!mqtt.ping()) {
-      mqtt.disconnect();
+    if (!estavaConectado && mqtt.connected()) {
+      for (int i = 0; i < NUM_VAGAS; i++) {
+        statusPublicado[i] = -1;
+      }
+      contadorPublicado = -1;
     }
 
-    unsigned long now = millis();
-    if (now - lastMsg > 10000 || now == 0) {
-      lastMsg = now;
-      publicarVagasStatus();
+    if (mqtt.connected()) {
+      mqtt.processPackets(1000);
+      if (!mqtt.ping()) {
+        mqtt.disconnect();
+        for (int i = 0; i < NUM_VAGAS; i++) {
+          statusPublicado[i] = -1;
+        }
+        contadorPublicado = -1;
+      }
     }
   } else {
+    for (int i = 0; i < NUM_VAGAS; i++) {
+      statusPublicado[i] = -1;
+    }
+    contadorPublicado = -1;
     Serial.println("WiFi offline; waiting for connection...");
     delay(1000);
   }
@@ -228,7 +291,9 @@ void loop() {
     float distance = duration == 0 ? 999 : duration * 0.034 / 2;
 
     bool ocupada = distance < 336;
-    vagaOcupada[i] = ocupada;
+    if (!vagaBloqueada[i]) {
+      vagaOcupada[i] = ocupada;
+    }
     ocupada = vagaOcupada[i];
 
     if (ocupada) {
@@ -254,16 +319,21 @@ void loop() {
     delay(50);
   }
 
-  int vagasLivres = numVagasComuns - vagasComunsOcupadas;
+  int vagasComunsLivres = numVagasComuns - vagasComunsOcupadas;
   int vagasIdosoLivres = numVagasIdoso - vagasIdosoOcupadas;
   int vagasPcdLivres = numVagasPcd - vagasPcdOcupadas;
   int vagasOcupadas = vagasComunsOcupadas + vagasIdosoOcupadas + vagasPcdOcupadas;
+  int vagasLivres = vagasComunsLivres + vagasIdosoLivres + vagasPcdLivres;
+
+  if (WiFi.status() == WL_CONNECTED && mqtt.connected()) {
+    publicarAlteracoes(vagasLivres);
+  }
 
   display.setCursor(4, 0);
-  if (vagasLivres < 10) {
+  if (vagasComunsLivres < 10) {
     display.print(' ');
   }
-  display.print(vagasLivres);
+  display.print(vagasComunsLivres);
 
   display.setCursor(12, 0);
   if (vagasOcupadas < 10) {
